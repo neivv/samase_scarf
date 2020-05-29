ae1125dce2b31529ac375cad0d9f3fd7d90e1e51
Markus Heikkinen <ittevien@gmail.com>
2020-05-30 02:13:50 +0300

    1234b

--- tests/everything.rs
+++ tests/everything.rs
@@ -981,6 +981,12 @@ fn everything_1234a() {
     })
 }
 
+#[test]
+fn everything_1234b() {
+    test_with_extra_checks(Path::new("1234b.exe"), |_ctx, _analysis| {
+    })
+}
+
 fn test(path: &Path) {
     test_with_extra_checks(path, |_, _| {});
 }
