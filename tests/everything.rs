8fda93442ce873f7036deec4943ceea43dc59b78
Markus Heikkinen <ittevien@gmail.com>
2020-10-09 22:06:06 +0300

    1236b

--- tests/everything.rs
+++ tests/everything.rs
@@ -1143,6 +1143,12 @@ fn everything_1236a() {
     })
 }
 
+#[test]
+fn everything_1236b() {
+    test_with_extra_checks(Path::new("1236b.exe"), |_ctx, _analysis| {
+    })
+}
+
 fn test(path: &Path) {
     test_with_extra_checks(path, |_, _| {});
 }
